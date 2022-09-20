a860fc8fe43a5a522adee8b613ee8d2b30149641
ramatoulaye diouf <ramatoulayediouf92@gmail.com>
2022-09-20 17:07:14 +0000

    ajouter

---
+++ index.js
@@ -0,0 +1,52 @@
+
+document.body.onload=function(){
+    nbr=5;
+    p=0;
+    container=document.getElementById("container");
+    g=document.getElementById("g");
+    d=document.getElementById("d");
+    container.style.width=(800*nbr)+"px";
+    for(i=1;i<=nbr;i++){
+        div=document.createElement("div");
+        div.className="photo";
+        div.style.backgroundImage="url('photos/im"+i+".jpg')";
+        container.appendChild(div);
+    }
+    afficherMasquer();
+}
+g.onclick=function(){
+    if(p>-nbr+1)
+    p--;
+    container.style.transform="translate("+p*800+"px)";
+    container.style.transition="all 0.5s ease";
+    afficherMasquer();
+}
+d.onclick=function(){
+    if(p<0)
+    p++;
+    container.style.transform="translate("+p*800+"px)";
+    container.style.transition="all 0.5s ease";
+    afficherMasquer();
+}
+     function afficherMasquer(){
+      if(p==-nbr+1)
+      g.style.visibility="hidden";
+      else
+      g.style.visibility="visible";
+
+      if(p==0)
+      d.style.visibility="hidden";
+      else
+      d.style.visibility="visible";
+    }
+    var menu_togle = document.querySelector('.menu_togle');
+var menu = document.querySelector('.menu');
+var  menu_togle_span = document.querySelector('.menu_togle span');
+
+    menu_togle.onclick = function(){
+    menu_togle.classList.toggle('active');
+    menu_togle_span.classList.toggle('active');
+    menu.classList.toggle('responsive');
+}
+    
+    
